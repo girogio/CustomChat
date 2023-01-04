437124a7e74956ea51ffa81464583e6a70993240
Giorgio Grigolo <giorgiogrigolo03@gmail.com>
2023-01-04 17:44:24 +0100

    Added options screen

---
+++ src/main/java/com/girogio2103/client/gui/screens/OptionScreen.java
@@ -0,0 +1,126 @@
+package com.girogio2103.client.gui.screens;
+
+import com.girogio2103.client.config.CustomChatClientConfig;
+import com.girogio2103.common.MqttConnection;
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.components.EditBox;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TextComponent;
+import org.jetbrains.annotations.NotNull;
+
+public class OptionScreen extends Screen {
+
+    private EditBox topicEditBox;
+    private EditBox brokerEditBox;
+
+    private Button saveButton;
+
+
+    public OptionScreen(Component title) {
+        super(title);
+    }
+
+    @Override
+    protected void init() {
+        super.init();
+
+        brokerEditBox = new EditBox(this.font, this.width / 2 - 100, this.height / 2 - 60, 200, 20, new TextComponent("Broker"));
+        brokerEditBox.setValue(CustomChatClientConfig.MQTT_BROKER.get());
+        this.addRenderableWidget(brokerEditBox);
+
+        topicEditBox = new EditBox(this.font, this.width / 2 - 100, this.height / 2 - 20, 200, 20, new TextComponent("Topic"));
+        topicEditBox.setValue(CustomChatClientConfig.MQTT_TOPIC.get());
+        this.addRenderableWidget(topicEditBox);
+
+
+        saveButton = new Button(this.width / 2 - 100, this.height / 2 + 10, 200, 20, new TextComponent("Save"), (button) -> {
+
+            CustomChatClientConfig.MQTT_BROKER.set(brokerEditBox.getValue());
+            CustomChatClientConfig.MQTT_TOPIC.set(topicEditBox.getValue());
+
+            if (MqttConnection.isConnected()) {
+                MqttConnection.disconnect();
+            } else {
+                MqttConnection.connect();
+            }
+        });
+
+
+        this.addRenderableWidget(saveButton);
+
+        this.addRenderableWidget(new Button(this.width / 2 - 100, this.height / 2 + 35, 200, 20, new TextComponent("Cancel"), (button) -> this.onClose()));
+
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+
+        if (this.topicEditBox.isFocused()) {
+            this.topicEditBox.tick();
+        }
+
+        if (this.brokerEditBox.isFocused()) {
+            this.brokerEditBox.tick();
+        }
+
+        saveButton.setMessage(new TextComponent(!MqttConnection.isConnected() ? "Connect" : "Disconnect"));
+
+        String urlRegex = "^(ws|tcp)://[a-zA-Z0-9]+(.[a-zA-Z0-9]+)*(:[0-9]{1,5})?$";
+        if (this.brokerEditBox.getValue().matches(urlRegex)) {
+            brokerEditBox.setTextColor(0xFFFFFF);
+            saveButton.active = true;
+        } else {
+            saveButton.setMessage(new TextComponent("Invalid URL"));
+            brokerEditBox.setTextColor(0xFF5555);
+            saveButton.active = false;
+        }
+
+        if (this.brokerEditBox.getValue().isEmpty()) {
+            this.brokerEditBox.setSuggestion(CustomChatClientConfig.MQTT_BROKER.get());
+        } else {
+            this.brokerEditBox.setSuggestion("");
+        }
+
+        if (this.topicEditBox.getValue().isEmpty()) {
+            this.topicEditBox.setSuggestion(CustomChatClientConfig.MQTT_TOPIC.get());
+        } else {
+            this.topicEditBox.setSuggestion("");
+        }
+    }
+
+    @Override
+    public void render(@NotNull PoseStack pose, int mouseX, int mouseY, float partialTick) {
+        // Background is typically rendered first
+        this.renderBackground(pose);
+
+        // Render things here before widgets (background textures)
+
+        // Then the widgets if this is a direct child of the Screen
+        super.render(pose, mouseX, mouseY, partialTick);
+
+        // Render things after widgets (tooltips)
+        drawString(pose, this.font, "Broker", this.width / 2 - 100, this.height / 2 - 70, 0xFFFFFF);
+        drawString(pose, this.font, "Topic", this.width / 2 - 100, this.height / 2 - 30, 0xFFFFFF);
+    }
+
+    @Override
+    public void onClose() {
+        // Stop any handlers here
+
+        // Call last in case it interferes with the override
+        super.onClose();
+    }
+
+    @Override
+    public void removed() {
+        // Reset initial states here
+        assert this.minecraft != null;
+        this.minecraft.keyboardHandler.setSendRepeatsToGui(false);
+
+        // Call last in case it interferes with the override
+        super.removed();
+    }
+}
