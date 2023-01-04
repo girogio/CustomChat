f6c663b27f54456509db29a722a75d72baf3c655
Giorgio Grigolo <giorgiogrigolo03@gmail.com>
2023-01-04 17:47:45 +0100

    Added menu toggle keymap

--- src/main/java/com/girogio2103/client/KeyInit.java
+++ src/main/java/com/girogio2103/client/KeyInit.java
@@ -10,12 +10,15 @@ public class KeyInit {
     public static KeyMapping toggleChat;
     public static KeyMapping toggleIndicator;
 
+    public static KeyMapping toggleMenu;
+
     private KeyInit() {
     }
 
     public static void init(){
         toggleChat = registerKey("mod.custom_chat.key.toggle", GLFW.GLFW_KEY_C, "key.categories.multiplayer");
         toggleIndicator = registerKey("mod.custom_chat.key.toggle_indicator", GLFW.GLFW_KEY_V, "key.categories.multiplayer");
+        toggleMenu = registerKey("mod.custom_chat.key.toggle_menu", GLFW.GLFW_KEY_M, "key.categories.multiplayer");
     }
 
     private static KeyMapping registerKey(String name, int keyCode, String category) {
